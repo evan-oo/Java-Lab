86b3b57fd27f51111872418d0179c847bce00d69
evan-oo <alberttamtamtamtam@gmail.com>
2022-02-24 02:12:59 +0800

    lab2

---
+++ src/base/ImageNote.java
@@ -0,0 +1,12 @@
+package base;
+
+import java.io.File; 
+
+public class ImageNote extends Note{
+	
+	public File image ;
+	
+	public ImageNote(String title) {
+		super(title) ;
+	}
+}
---
+++ src/base/TextNote.java
@@ -0,0 +1,10 @@
+package base;
+
+public class TextNote extends Note{
+
+	public String content ;
+	
+	public TextNote(String title) {
+		super(title) ;
+	}
+}
